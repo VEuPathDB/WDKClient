8e84b5b95aff72b9ae33f6d604585ee1ae894746
Jamie Long <jtlong3rd@gmail.com>
2018-12-12 19:13:21 +0000

    Modest enhancement to Tabs component to support removable tabs and ReactNode display names
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDKClient/branches/stepAnalysisRefactor@23540 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Client/src/Components/Tabs/Tabs.tsx
+++ Client/src/Components/Tabs/Tabs.tsx
@@ -1,22 +1,25 @@
 import './Tabs.scss';
 
-import React from 'react';
+import React, { Fragment } from 'react';
 import { makeClassNameHelper } from '../../Utils/ComponentUtils';
 
 const cx = makeClassNameHelper('wdk-Tab');
 
-type TabConfig = {
+export type TabConfig = {
   key: string;
-  display: string;
+  display: React.ReactNode;
+  removable?: boolean;
+  tooltip?: string;
   content: React.ReactNode;
-}
+};
 
 type Props = {
   tabs: TabConfig[];
   activeTab: string;
   onTabSelected: (tab: string) => void;
+  onTabRemoved?: (tab: string) => void;
   className?: string;
-}
+};
 
 export default function Tabs(props: Props) {
   const activeTab = props.tabs.find(tab => tab.key === props.activeTab);
@@ -24,8 +27,30 @@ export default function Tabs(props: Props) {
     <div className={cx('sContainer')}>
       <div className={cx('s')}>
         {props.tabs.map(tab => (
-          <button type="button" key={tab.key} onClick={() => props.onTabSelected(tab.key)} className={cx('', activeTab === tab ? 'active' : '')}>
+          <button 
+            title={tab.tooltip}
+            type="button" 
+            key={tab.key} 
+            onClick={() => props.onTabSelected(tab.key)} 
+            className={cx('', activeTab === tab ? 'active' : '')}
+          >
             {tab.display}
+            {
+              tab.removable &&
+              <Fragment>
+                {' '}
+                <i 
+                  className="fa fa-times" 
+                  onClick={(event) => {
+                      event.stopPropagation();
+                      if (props.onTabRemoved) {
+                        props.onTabRemoved(tab.key);
+                      }
+                    }
+                  }
+                />
+              </Fragment>
+            }
           </button>
         ))}
       </div>
